27614bc772eda0ca89d5a683d398fbe4bc5cff76
Nando Vieira <fnando.vieira@gmail.com>
2011-09-19 21:22:18 -0300

    Load files in specific order

--- init.sh
+++ init.sh
@@ -1,3 +1,4 @@
+source $HOME/.bash/scripts/other.sh
 source $HOME/.bash/scripts/export.sh
 source $HOME/.bash/scripts/alias.sh
 source $HOME/.bash/scripts/options.sh
@@ -5,7 +6,6 @@ source $HOME/.bash/scripts/options.sh
 source $HOME/.bash/scripts/git_completion.sh
 source $HOME/.bash/scripts/bash_completion.sh
 source $HOME/.bash/scripts/gem_completion.sh
-source $HOME/.bash/scripts/other.sh
 source $HOME/.bash/scripts/utils.sh
 
 source $HOME/.bash/scripts/prompt.sh
