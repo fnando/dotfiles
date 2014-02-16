406095e104fec41db69368f4ca94a9e8cb112894
Nando Vieira <fnando.vieira@gmail.com>
2014-02-16 12:50:15 -0300

    Added some `less` configuration.

--- .bash/general.sh
+++ .bash/general.sh
@@ -20,6 +20,13 @@ export GEM_HOME=$INSTALL_DIR/ruby/gems
 export GEM_PATH=$INSTALL_DIR/ruby/gems
 export CDHISTORY="/tmp/cd-$USER"
 export LESS="-REX"
+export LESS_TERMCAP_mb=$'\E[04;33m'
+export LESS_TERMCAP_md=$'\E[04;33m'
+export LESS_TERMCAP_me=$'\E[0m'
+export LESS_TERMCAP_se=$'\E[0m'
+export LESS_TERMCAP_so=$'\E[01;44;33m'
+export LESS_TERMCAP_ue=$'\E[0m'
+export LESS_TERMCAP_us=$'\E[00;32m'
 
 for option in autocd cdspell cmdhist dotglob extblog dirspell globstar histappend nocaseglob no_empty_cmd_completion checkwinsize; do
   tmp="$(shopt -q "$option" 2>&1 > /dev/null | grep "invalid shell option name")"
