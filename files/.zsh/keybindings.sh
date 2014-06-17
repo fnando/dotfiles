8700b850d8754ce6c85ef1f5711c28f8752e7f22
Nando Vieira <fnando.vieira@gmail.com>
2014-06-17 14:45:44 -0300

    Add keymapping for fn-delete.

--- files/.zsh/keybindings.sh
+++ files/.zsh/keybindings.sh
@@ -9,3 +9,4 @@ zle -N down-line-or-beginning-search
 bindkey "^[[A" up-line-or-beginning-search # Up
 bindkey "^[[B" down-line-or-beginning-search # Down
 bindkey \^U backward-kill-line  # Ctrl+U
+bindkey "^[[3~" delete-char # fn-delete
