371e1329019e120d29b1b9632ab8c0cf5690a629
Nando Vieira <fnando.vieira@gmail.com>
2014-06-20 15:13:51 -0300

    Remove unused code.

--- files/common/aliases.sh
+++ files/common/aliases.sh
@@ -4,16 +4,3 @@ alias pcat="pygmentize -f terminal256 -O style=tango -g"
 alias hex="openssl rand -hex"
 alias path='echo $PATH | tr : "\n" | uniq'
 alias pubkey="cat ~/.ssh/id_rsa.pub | pbcopy | printf '=> Public key copied to pasteboard.\n'"
-
-psgrep() {
-  output=$(ps aux)
-  header=$(echo $output | head -n1)
-  matches=$(echo -n $output | grep -v grep | grep)
-
-  if [[ "$matches" != "" ]]; then
-    echo $header
-    echo $matches
-  fi
-}
-
-# alias psgrep="ps aux | grep -v grep | grep"
