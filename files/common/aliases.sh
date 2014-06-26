cba147ccc3a5a1024442296e24587f309f0d3f0d
Nando Vieira <fnando.vieira@gmail.com>
2014-06-26 19:26:33 -0300

    Add some new aliases.

--- files/common/aliases.sh
+++ files/common/aliases.sh
@@ -5,3 +5,5 @@ alias hex="openssl rand -hex"
 alias path='echo $PATH | tr : "\n" | uniq'
 alias pubkey="cat ~/.ssh/id_rsa.pub | pbcopy | printf '=> Public key copied to pasteboard.\n'"
 alias subl="subl -n"
+alias h="heroku"
+alias c="codeplane"
---
+++ files/common/foreman.sh
@@ -0,0 +1,5 @@
+export BUNDLE_JOBS=3
+
+alias fr="foreman run"
+alias fs="foreman start"
+alias fbe="foreman bundle exec"
