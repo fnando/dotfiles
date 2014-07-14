a96ac65078b1095d3ea9df49e83de14b1f259408
Nando Vieira <fnando.vieira@gmail.com>
2014-07-14 18:18:56 -0300

    Set more Git variables.

--- files/common/git.sh
+++ files/common/git.sh
@@ -2,5 +2,7 @@ if [[ "$GIT_AUTHOR_NAME" = "" ]]; then
   (echo Please configure ~/.${SHELL_NAME}/user.sh with your git info.) > /dev/stderr
 fi
 
-git config --global user.name "$GIT_AUTHOR_NAME"
-git config --global user.email "$GIT_AUTHOR_EMAIL"
+if [[ "$(git config --get user.name)" != "$GIT_AUTHOR_NAME" ]]; then
+  git config --global user.name "$GIT_AUTHOR_NAME"
+  git config --global user.email "$GIT_AUTHOR_EMAIL"
+fi
--- files/common/user.sh
+++ files/common/user.sh
@@ -1,3 +1,5 @@
 # Set your Git commit info.
 # export GIT_AUTHOR_NAME="Your name"
 # export GIT_AUTHOR_EMAIL="Your e-mail"
+# export GIT_COMMITTER_NAME=$GIT_AUTHOR_NAME
+# export GIT_COMMITTER_EMAIL=$GIT_AUTHOR_EMAIL
