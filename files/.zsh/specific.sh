4d99a9bbb5f2056009c56925e6424429c737ab94
Nando Vieira <fnando.vieira@gmail.com>
2014-12-14 08:11:55 -0800

    Ignore zsh globbing on some commands.

--- files/.zsh/specific.sh
+++ files/.zsh/specific.sh
@@ -1,3 +1,7 @@
 export HISTFILE="$HOME/.zsh_history"
 WORDCHARS=${WORDCHARS//[&=\/;\!#%\{]}
 alias reload="source ~/.zshrc"
+
+[ -x `which git` ] && alias git="noglob git"
+[ -x `which npm` ] && alias npm="noglob npm"
+[ -x `which bower` ] && alias bower="noglob bower"
