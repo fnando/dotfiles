1041e008b1152bed9f58b7f85c32ab6d58cd3ba3
Nando Vieira <fnando.vieira@gmail.com>
2014-12-17 11:21:14 -0200

    Fix expression.

--- files/.zsh/specific.sh
+++ files/.zsh/specific.sh
@@ -2,6 +2,6 @@ export HISTFILE="$HOME/.zsh_history"
 WORDCHARS=${WORDCHARS//[&=\/;\!#%\{]}
 alias reload="source ~/.zshrc"
 
-[ -x `which git` ] && alias git="noglob git"
-[ -x `which npm` ] && alias npm="noglob npm"
-[ -x `which bower` ] && alias bower="noglob bower"
+[[ -x `which git` ]] && alias git="noglob git"
+[[ -x `which npm` ]] && alias npm="noglob npm"
+[[ -x `which bower` ]] && alias bower="noglob bower"
