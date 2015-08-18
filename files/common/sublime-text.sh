069e5fd23fdc17c353e95df1b0aa793dcdc9a698
Nando Vieira <fnando.vieira@gmail.com>
2015-08-18 11:19:14 -0300

    Replace all values.

--- files/common/sublime-text.sh
+++ files/common/sublime-text.sh
@@ -2,4 +2,4 @@ export BUNDLER_EDITOR=subl
 export GEM_EDITOR="subl"
 export EDITOR="subl -w"
 export VISUAL="$EDITOR"
-git config --global core.editor $EDITOR
+git config --replace-all --global core.editor $EDITOR
