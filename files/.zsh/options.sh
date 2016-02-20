d37321fdcfd94537fc618b5fb555f5643e324796
Nando Vieira <fnando.vieira@gmail.com>
2016-02-20 16:07:59 -0200

    Disable ZSH's global configuration loading.
    This is required because El Capitan now uses /usr/libexec/path_helper on a global file (/etc/zprofile), overriding user's configuration.

--- files/.zsh/options.sh
+++ files/.zsh/options.sh
@@ -1,30 +1,31 @@
 bindkey -e
 cdpath=( . /vagrant /Projects ~ ~/Projects )
-setopt nobeep
-setopt nohistbeep
-setopt nolistbeep
 setopt always_to_end
 setopt auto_menu
 setopt auto_name_dirs
 setopt auto_pushd
-setopt autocd
+setopt auto_cd
 setopt complete_in_word
-unsetopt correctall
-setopt extendedglob
-setopt extendedhistory
-setopt histfindnodups
-setopt histignorealldups
-setopt histreduceblanks
-setopt histsavenodups
-setopt histignorespace
-setopt incappendhistory
+setopt extended_glob
+setopt extended_history
+setopt hist_find_no_dups
+setopt hist_ignore_all_dups
+setopt hist_ignore_space
+setopt hist_reduce_blanks
+setopt hist_save_no_dups
+setopt inc_append_history
 setopt long_list_jobs
+setopt no_beep
+setopt no_global_rcs
+setopt no_hist_beep
+setopt no_list_beep
+setopt prompt_subst
 setopt pushd_ignore_dups
-setopt pushdminus
-setopt promptsubst
-setopt sharehistory
+setopt pushd_minus
+setopt share_history
+unsetopt correct_all
 unsetopt menu_complete
-unsetopt flowcontrol
+unsetopt flow_control
 zstyle ':completion:*' special-dirs true
 zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
 autoload -U compinit
