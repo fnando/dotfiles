d5dc388503028852736b1c8c2903134f9175e8c9
Nando Vieira <fnando.vieira@gmail.com>
2016-03-30 22:58:53 -0300

    Tweak git config.

--- files/common/git.sh
+++ files/common/git.sh
@@ -1,16 +1,25 @@
-if [[ "$GIT_AUTHOR_NAME" = "" ]]; then
+author_name=$(git config --get user.name)
+author_email=$(git config --get user.email)
+author_signingkey=$(git config --get user.signingkey)
+
+if [[ "$author_name" = "" ]]; then
   (echo Please configure ~/.${SHELL_NAME}/user.sh with your git info.) > /dev/stderr
 fi
 
-if [[ "$(git config --get user.name)" != "$GIT_AUTHOR_NAME" ]]; then
+if [[ "$(git config --global --get user.name)" != "$GIT_AUTHOR_NAME" ]]; then
   git config --global user.name "$GIT_AUTHOR_NAME"
   git config --global user.email "$GIT_AUTHOR_EMAIL"
 fi
 
-if [[ "$GIT_SIGNING_KEY" != "" ]]; then
+if [[ "$GIT_AUTHOR_SIGNING_KEY" != "" ]]; then
   git config --global user.signingkey "$GIT_SIGNING_KEY"
 fi
 
 if [[ "$GIT_DIFF_HIGHLIGHT" != "" ]]; then
   git config --global core.pager "$GIT_DIFF_HIGHLIGHT | less"
 fi
+
+unset GIT_DIFF_HIGHLIGHT
+unset GIT_AUTHOR_NAME
+unset GIT_AUTHOR_EMAIL
+unset GIT_AUTHOR_SIGNING_KEY
--- files/common/user.sh
+++ files/common/user.sh
@@ -1,5 +1,6 @@
 # Set your Git commit info.
 # export GIT_AUTHOR_NAME="Your name"
 # export GIT_AUTHOR_EMAIL="Your e-mail"
+# export GIT_AUTHOR_SIGNING_KEY="GPG ID"
 # export GIT_COMMITTER_NAME=$GIT_AUTHOR_NAME
 # export GIT_COMMITTER_EMAIL=$GIT_AUTHOR_EMAIL
