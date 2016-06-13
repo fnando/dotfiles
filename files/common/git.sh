33889169f24776053be5ccf6bbca87dbeb045e5d
Nando Vieira <fnando.vieira@gmail.com>
2016-06-13 13:34:42 -0700

    Always set git values.

--- files/common/git.sh
+++ files/common/git.sh
@@ -1,19 +1,14 @@
-author_name=$(git config --get user.name)
-author_email=$(git config --get user.email)
-author_signingkey=$(git config --get user.signingkey)
+author_name=${GIT_AUTHOR_NAME:-`git config --get user.name`}
+author_email=${GIT_AUTHOR_EMAIL:-`git config --get user.email`}
+author_signingkey=${GIT_AUTHOR_SIGNING_KEY:-`git config --get user.signingkey`}
 
 if [[ "$author_name" = "" ]]; then
   (echo Please configure ~/.${SHELL_NAME}/user.sh with your git info.) > /dev/stderr
 fi
 
-if [[ "$(git config --global --get user.name)" != "$GIT_AUTHOR_NAME" ]]; then
-  git config --global user.name "$GIT_AUTHOR_NAME"
-  git config --global user.email "$GIT_AUTHOR_EMAIL"
-fi
-
-if [[ "$GIT_AUTHOR_SIGNING_KEY" != "" ]]; then
-  git config --global user.signingkey "$GIT_AUTHOR_SIGNING_KEY"
-fi
+git config --global user.name "$author_name"
+git config --global user.email "$author_email"
+git config --global user.signingkey "$author_signingkey"
 
 if [[ "$GIT_DIFF_HIGHLIGHT" != "" ]]; then
   git config --global core.pager "$GIT_DIFF_HIGHLIGHT | less"
