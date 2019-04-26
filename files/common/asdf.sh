aeb2387872d66912dc336979c71e64c181f4af5c
Nando Vieira <me@fnando.com>
2019-04-25 23:51:23 -0700

    Fix asdf detection on macOS.

--- files/common/asdf.sh
+++ files/common/asdf.sh
@@ -1,5 +1,6 @@
-if [[ -x "brew" ]]; then
-  asdf_file="/usr/local/opt/asdf/asdf.sh"
+if [[ -x "$(which brew)" ]]; then
+  export ASDF_DIR=$(brew --prefix asdf)
+  asdf_file="$ASDF_DIR/asdf.sh"
   asdf_completion_file="$(brew --prefix)/etc/bash_completion.d/asdf.bash"
 else
   asdf_file="$HOME/.asdf/asdf.sh"
