d2b8a10fd7a2f3a70a0a2a41ba658f8c50c04a2f
Nando Vieira <me@fnando.com>
2020-04-08 09:41:42 -0700

    ASDF now comes with a proper ZSH completion script.

--- files/common/asdf.sh
+++ files/common/asdf.sh
@@ -1,10 +1,8 @@
 if [[ -x "$(which brew)" ]]; then
   export ASDF_DIR=$(brew --prefix asdf)
   asdf_file="$ASDF_DIR/asdf.sh"
-  asdf_completion_file="$(brew --prefix)/etc/bash_completion.d/asdf.bash"
 else
   asdf_file="$HOME/.asdf/asdf.sh"
-  asdf_completion_file="$HOME/.asdf/completions/asdf.bash"
 fi
 
 if [[ -f "$asdf_file" ]]; then
@@ -13,5 +11,4 @@ if [[ -f "$asdf_file" ]]; then
   unset NPM_HOME
   unset NODE_PATH
   source "$asdf_file"
-  source "$asdf_completion_file"
 fi
