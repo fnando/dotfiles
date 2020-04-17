490095125ee440c8624ea01673db241d976db4d6
Nando Vieira <me@fnando.com>
2020-04-17 09:46:44 -0700

    Add script that adds Time Machine backup exclusions.

---
+++ files/timemachine.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+# This file sets exclusions for Time Machine backups.
+
+sudo tmutil addexclusion -p /Applications/Android\ Studio.app
+sudo tmutil addexclusion -p /Applications/Brave\ Browser.app
+sudo tmutil addexclusion -p /Applications/Firefox.app
+sudo tmutil addexclusion -p /Applications/Google\ Chrome.app
+sudo tmutil addexclusion -p /Applications/Keynote.app
+sudo tmutil addexclusion -p /Applications/Logic\ Pro\ X.app
+sudo tmutil addexclusion -p /Applications/Logitech
+sudo tmutil addexclusion -p /Applications/Motion.app
+sudo tmutil addexclusion -p /Applications/Music.app
+sudo tmutil addexclusion -p /Applications/Numbers.app
+sudo tmutil addexclusion -p /Applications/Opera.app
+sudo tmutil addexclusion -p /Applications/Pages.app
+sudo tmutil addexclusion -p /Applications/Safari.app
+sudo tmutil addexclusion -p /Applications/Safari\ Technology\ Preview.app
+sudo tmutil addexclusion -p /Applications/Slack.app
+sudo tmutil addexclusion -p /Applications/Spotify.app
+sudo tmutil addexclusion -p /Applications/TV.app
+sudo tmutil addexclusion -p /Applications/VirtualBox.app
+sudo tmutil addexclusion -p /Applications/Xcode.app
+sudo tmutil addexclusion -p /Library
+sudo tmutil addexclusion -p /private
+sudo tmutil addexclusion -p /System
+sudo tmutil addexclusion -p /usr/local
+sudo tmutil addexclusion -p ~/.android
+sudo tmutil addexclusion -p ~/.asdf
+sudo tmutil addexclusion -p ~/.cache
+sudo tmutil addexclusion -p ~/.cocoapods
+sudo tmutil addexclusion -p ~/.electron
+sudo tmutil addexclusion -p ~/.gradle
+sudo tmutil addexclusion -p ~/.npm
+sudo tmutil addexclusion -p ~/Downloads
+sudo tmutil addexclusion -p ~/Library/Android
+sudo tmutil addexclusion -p ~/Library/Application\ Support/Dash
+sudo tmutil addexclusion -p ~/Library/Application\ Support/Google
+sudo tmutil addexclusion -p ~/Library/Application\ Support/Keybase
+sudo tmutil addexclusion -p ~/Library/Application\ Support/Slack
+sudo tmutil addexclusion -p ~/Library/Application\ Support/Spotify
+sudo tmutil addexclusion -p ~/Library/Caches
+sudo tmutil addexclusion -p ~/Library/Developer
+sudo tmutil addexclusion -p ~/Library/Logs
+sudo tmutil addexclusion -p ~/Library/Logs
+sudo tmutil addexclusion -p ~/VirtualBox\ VMs
