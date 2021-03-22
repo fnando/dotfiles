b9f65febc0010e2d738c183d0a5292c92cf0426c
Nando Vieira <me@fnando.com>
2021-03-22 01:32:18 -0700

    Update list of excluded files from Time Machine.

--- files/timemachine.sh
+++ files/timemachine.sh
@@ -1,46 +1,82 @@
 #!/usr/bin/env bash
 
 # This file sets exclusions for Time Machine backups.
+# You must run this file with `sudo timemachine.sh`.
 
-sudo tmutil addexclusion -p /Applications/Android\ Studio.app
-sudo tmutil addexclusion -p /Applications/Brave\ Browser.app
-sudo tmutil addexclusion -p /Applications/Firefox.app
-sudo tmutil addexclusion -p /Applications/Google\ Chrome.app
-sudo tmutil addexclusion -p /Applications/Keynote.app
-sudo tmutil addexclusion -p /Applications/Logic\ Pro\ X.app
-sudo tmutil addexclusion -p /Applications/Logitech
-sudo tmutil addexclusion -p /Applications/Motion.app
-sudo tmutil addexclusion -p /Applications/Music.app
-sudo tmutil addexclusion -p /Applications/Numbers.app
-sudo tmutil addexclusion -p /Applications/Opera.app
-sudo tmutil addexclusion -p /Applications/Pages.app
-sudo tmutil addexclusion -p /Applications/Safari.app
-sudo tmutil addexclusion -p /Applications/Safari\ Technology\ Preview.app
-sudo tmutil addexclusion -p /Applications/Slack.app
-sudo tmutil addexclusion -p /Applications/Spotify.app
-sudo tmutil addexclusion -p /Applications/TV.app
-sudo tmutil addexclusion -p /Applications/VirtualBox.app
-sudo tmutil addexclusion -p /Applications/Xcode.app
-sudo tmutil addexclusion -p /Library
-sudo tmutil addexclusion -p /private
-sudo tmutil addexclusion -p /System
-sudo tmutil addexclusion -p /usr/local
-sudo tmutil addexclusion -p ~/.android
-sudo tmutil addexclusion -p ~/.asdf
-sudo tmutil addexclusion -p ~/.cache
-sudo tmutil addexclusion -p ~/.cocoapods
-sudo tmutil addexclusion -p ~/.electron
-sudo tmutil addexclusion -p ~/.gradle
-sudo tmutil addexclusion -p ~/.npm
-sudo tmutil addexclusion -p ~/Downloads
-sudo tmutil addexclusion -p ~/Library/Android
-sudo tmutil addexclusion -p ~/Library/Application\ Support/Dash
-sudo tmutil addexclusion -p ~/Library/Application\ Support/Google
-sudo tmutil addexclusion -p ~/Library/Application\ Support/Keybase
-sudo tmutil addexclusion -p ~/Library/Application\ Support/Slack
-sudo tmutil addexclusion -p ~/Library/Application\ Support/Spotify
-sudo tmutil addexclusion -p ~/Library/Caches
-sudo tmutil addexclusion -p ~/Library/Developer
-sudo tmutil addexclusion -p ~/Library/Logs
-sudo tmutil addexclusion -p ~/Library/Logs
-sudo tmutil addexclusion -p ~/VirtualBox\ VMs
+tmutil addexclusion -p /Applications
+tmutil addexclusion -p /Library
+tmutil addexclusion -p /opt/homebrew
+tmutil addexclusion -p /private
+tmutil addexclusion -p /System
+tmutil addexclusion -p /usr/local
+tmutil addexclusion -p ~/.android
+tmutil addexclusion -p ~/.asdf
+tmutil addexclusion -p ~/.bundle
+tmutil addexclusion -p ~/.cache
+tmutil addexclusion -p ~/.cocoapods
+tmutil addexclusion -p ~/.composer
+tmutil addexclusion -p ~/.electron
+tmutil addexclusion -p ~/.gradle
+tmutil addexclusion -p ~/.local
+tmutil addexclusion -p ~/.m2
+tmutil addexclusion -p ~/.node-gyp
+tmutil addexclusion -p ~/.npm
+tmutil addexclusion -p ~/.nvm
+tmutil addexclusion -p ~/.solargraph
+tmutil addexclusion -p ~/.vagrant.d
+tmutil addexclusion -p ~/.vpython-root
+tmutil addexclusion -p ~/.vscode
+tmutil addexclusion -p ~/.vscode-insiders
+tmutil addexclusion -p ~/Downloads
+tmutil addexclusion -p ~/Library/Android
+tmutil addexclusion -p ~/Library/Application\ Support/Adobe
+tmutil addexclusion -p ~/Library/Application\ Support/Bartalina
+tmutil addexclusion -p ~/Library/Application\ Support/Blackmagic\ Design
+tmutil addexclusion -p ~/Library/Application\ Support/BraveSoftware
+tmutil addexclusion -p ~/Library/Application\ Support/Caches
+tmutil addexclusion -p ~/Library/Application\ Support/CleanShot
+tmutil addexclusion -p ~/Library/Application\ Support/Code\ -\ Insiders
+tmutil addexclusion -p ~/Library/Application\ Support/com.sonos.macController
+tmutil addexclusion -p ~/Library/Application\ Support/com.sonos.macController2
+tmutil addexclusion -p ~/Library/Application\ Support/Dash
+tmutil addexclusion -p ~/Library/Application\ Support/discord
+tmutil addexclusion -p ~/Library/Application\ Support/Firefox
+tmutil addexclusion -p ~/Library/Application\ Support/Google
+tmutil addexclusion -p ~/Library/Application\ Support/Imaging\ Edge\ Desktop
+tmutil addexclusion -p ~/Library/Application\ Support/InVision\ Studio
+tmutil addexclusion -p ~/Library/Application\ Support/JetBrains
+tmutil addexclusion -p ~/Library/Application\ Support/Keybase
+tmutil addexclusion -p ~/Library/Application\ Support/Knowledge
+tmutil addexclusion -p ~/Library/Application\ Support/kotlin
+tmutil addexclusion -p ~/Library/Application\ Support/Ledger\ Live
+tmutil addexclusion -p ~/Library/Application\ Support/Mozilla
+tmutil addexclusion -p ~/Library/Application\ Support/now
+tmutil addexclusion -p ~/Library/Application\ Support/nwjs
+tmutil addexclusion -p ~/Library/Application\ Support/OpenEmu
+tmutil addexclusion -p ~/Library/Application\ Support/org.videolan.vlc
+tmutil addexclusion -p ~/Library/Application\ Support/React\ Native\ Debugger
+tmutil addexclusion -p ~/Library/Application\ Support/Slack
+tmutil addexclusion -p ~/Library/Application\ Support/Sonos
+tmutil addexclusion -p ~/Library/Application\ Support/SonosV2
+tmutil addexclusion -p ~/Library/Application\ Support/Spotify
+tmutil addexclusion -p ~/Library/Application\ Support/zoom.us
+tmutil addexclusion -p ~/Library/Caches
+tmutil addexclusion -p ~/Library/Containers
+tmutil addexclusion -p ~/Library/Detox
+tmutil addexclusion -p ~/Library/Developer
+tmutil addexclusion -p ~/Library/Group\ Containers
+tmutil addexclusion -p ~/Library/Logs
+tmutil addexclusion -p ~/Library/Mail
+tmutil addexclusion -p ~/Library/Messages
+tmutil addexclusion -p ~/Library/Metadata
+tmutil addexclusion -p ~/Library/Mobile\ Documents
+tmutil addexclusion -p ~/Library/Safari
+tmutil addexclusion -p ~/Library/SafariTechnologyPreview
+tmutil addexclusion -p ~/Library/Screen\ Savers
+tmutil addexclusion -p ~/Library/WebKit
+tmutil addexclusion -p ~/Movies
+tmutil addexclusion -p ~/VirtualBox\ VMs
+
+find $HOME/Projects -name 'node_modules' -type d -depth 3 -exec tmutil addexclusion -p {} \;
+find $HOME/Projects -name '.gradle' -type d -depth 4 -exec tmutil addexclusion -p {} \;
+find $HOME/Projects -regex '.*/android/app/build' -type d -depth 5 -exec tmutil addexclusion -p {} \;
