5ed9577cd6d25a4e19f165688f34f794aead6089
Nando Vieira <me@fnando.com>
2024-11-23 17:11:05 -0800

    Move things around.

--- fnando/.config/zsh/go.sh
+++
@@ -1,5 +0,0 @@
-if [[ -x "$(which go)" ]]; then
-  source ~/.asdf/plugins/golang/set-env.zsh
-  export GOPATH="${GOPATH:-$HOME/Projects/golang}"
-  export PATH="$GOPATH/bin:$(go env GOROOT)/bin:$PATH"
-fi
