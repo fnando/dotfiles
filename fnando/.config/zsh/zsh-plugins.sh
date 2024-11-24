81958e39bf59f10ed107effba86587f4b9559555
Nando Vieira <me@fnando.com>
2024-11-24 15:12:19 -0800

    Autocomplete is not great with histdb.

--- fnando/.config/zsh/zsh-plugins.sh
+++ fnando/.config/zsh/zsh-plugins.sh
@@ -13,15 +13,3 @@ done
 
 # General plugin configuration
 export ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=white"
-
-_zsh_autosuggest_strategy_histdb_top_here() {
-    local query="select commands.argv from
-history left join commands on history.command_id = commands.rowid
-left join places on history.place_id = places.rowid
-where places.dir LIKE '$(sql_escape $PWD)%'
-and commands.argv LIKE '$(sql_escape $1)%'
-group by commands.argv order by count(*) desc limit 1"
-    suggestion=$(_histdb_query "$query")
-}
-
-ZSH_AUTOSUGGEST_STRATEGY=histdb_top_here
