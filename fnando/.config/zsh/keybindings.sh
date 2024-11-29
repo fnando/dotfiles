7b2af0f302c78a3483f9340c4259e5f89647eb18
Nando Vieira <me@fnando.com>
2024-11-29 09:07:18 -0800

    Update keybindings.

--- fnando/.config/zsh/keybindings.sh
+++ fnando/.config/zsh/keybindings.sh
@@ -1,19 +1,20 @@
 bindkey -e
-bindkey "^[b" emacs-forward-word # ctrl-left
-bindkey "^[f" emacs-backward-word # ctrl-right
-
 autoload -U up-line-or-beginning-search
 autoload -U down-line-or-beginning-search
 zle -N up-line-or-beginning-search
 zle -N down-line-or-beginning-search
+
+bindkey "^[b" emacs-backward-word # ctrl-right
+bindkey "^[f" emacs-forward-word # ctrl-left
+bindkey "^J" emacs-backward-word # ctrl-j
+bindkey "^K" emacs-forward-word # ctrl-k
 bindkey "^[[A" up-line-or-beginning-search # up
 bindkey "^[[B" down-line-or-beginning-search # down
 bindkey '^u' backward-kill-line  # ctrl-u
 bindkey '^d' kill-whole-line  # ctrl-d
-bindkey "\e[3~" delete-char # fn-delete
 bindkey "^r" history-incremental-search-backward
-bindkey "^j" history-search-backward
-bindkey "^k" history-search-forward
+bindkey "∆" history-search-backward # option-j
+bindkey "˚" history-search-forward # option-k
 bindkey '^a' beginning-of-line
 bindkey '^e' end-of-line
 bindkey '^w' backward-kill-word
