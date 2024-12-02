346f0938ddcd6dea84771f813963c6e31c058f98
Nando Vieira <me@fnando.com>
2024-12-02 09:24:54 -0800

    Remove notify plugin.

--- fnando/.config/zsh/zinit.sh
+++ fnando/.config/zsh/zinit.sh
@@ -5,7 +5,6 @@ _source-file-if-exists $HOMEBREW_PREFIX/opt/zinit/zinit.zsh
 zinit light zdharma-continuum/fast-syntax-highlighting
 zinit light zsh-users/zsh-syntax-highlighting
 zinit light zsh-users/zsh-autosuggestions
-zinit light MichaelAquilina/zsh-auto-notify
 
 _source-file-if-exists $HOMEBREW_PREFIX/Library/Taps/homebrew/homebrew-command-not-found/handler.sh
 
